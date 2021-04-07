1ec1cedbcd65e9150c8c078943ff9930e2b9e167
Bjorn Lottering (Volmalites) <bjorn@octonet.co.za>
2021-04-07 22:30:10 +0200

    Chat page initiating socket

---
+++ src/components/Chat.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="grid">
+        <section>
+            <h1>CHAT IS UNAVAILABLE, COME BACK SOON!</h1>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Services',
+  data () {
+    return {
+      isConnected: false
+    }
+  },
+  sockets: {
+    connect () {
+      this.isConnected = true
+    },
+    disconnect () {
+      this.isConnected = false
+    },
+    messageChannel (data) {
+      this.socketMessage = data
+    }
+  },
+  mounted() {
+    this.$socket.client.on('log', (result) => {
+      console.log(result)
+    })
+  }
+}
+</script>
+
+<style scoped>
+    
+</style>
